c3fc36a50107ea62586c2aefcf04fb83e8b86bd5
TheShepsMT <theshepsmt@gmail.com>
2024-04-26 19:24:00 -0500

    Improve welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     {{-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" /> --}}
 
     <main class="mt-6 m-20">
-    {{-- 
+        {{-- 
         <div role="alert" class="alert alert-info">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <span>New software update available.</span>
@@ -23,6 +23,7 @@
             <span>Error! Task failed successfully.</span>
           </div>
     --}}
+    <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100">Hover me</button>s
     </main>
 
 
@@ -30,40 +31,24 @@
 
     <footer class="footer p-10 bg-base text-base-content mt-auto" data-theme="wowc_neutral">
         <nav>
-            <h6 class="footer-title">Services</h6>
-            <a class="link link-hover">Branding</a>
-            <a class="link link-hover">Design</a>
-            <a class="link link-hover">Marketing</a>
-            <a class="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
-            <h6 class="footer-title">Company</h6>
+            <h6 class="footer-title">Team</h6>
             <a class="link link-hover">About us</a>
             <a class="link link-hover">Contact</a>
-            <a class="link link-hover">Jobs</a>
-            <a class="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+            <h6 class="footer-title">Useful Links</h6>
+            <a class="link link-hover" href="https://github.com/World-of-Warclass" target="_blank">Github</a>
+            <a class="link link-hover">Special Thanks</a>
+
         </nav>
         <nav>
             <h6 class="footer-title">Social</h6>
-            <div class="grid grid-flow-col gap-4">
-                <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        class="fill-current">
-                        <path
-                            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z">
-                        </path>
-                    </svg></a>
-                <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        class="fill-current">
-                        <path
-                            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z">
-                        </path>
-                    </svg></a>
-                <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        class="fill-current">
-                        <path
-                            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z">
-                        </path>
-                    </svg></a>
+            <div class="grid grid-flow-col gap-4 place-items-center text-2xl">
+                <a href="https://github.com/World-of-Warclass" target="_blank" class="transition-transform transform-growth hover:scale-110 duration-200"><span class="icon-[ri--github-fill]"></span></a>
+                <a href="#" class="transition-transform transform-growth hover:scale-110 duration-200"><span class="icon-[ri--facebook-circle-fill]"></span></a>
+                <a href="#" class="transition-transform transform-growth hover:scale-110 duration-200"><span class="icon-[ri--twitter-x-line]"></span></a>
+                <a href="#" class="transition-transform transform-growth hover:scale-110 duration-200"><span class="icon-[ri--youtube-fill]"></span></a>
+                <a href="#" class="transition-transform transform-growth hover:scale-110 duration-200"><span class="icon-[ri--soundcloud-line]"></span></a>
             </div>
         </nav>
     </footer>
