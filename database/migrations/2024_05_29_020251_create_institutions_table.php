ce2f6e8f5427796cd2e597828239770a843319dd
Mark_clap <blusito48@gmail.com>
2024-06-10 19:51:47 -0500

    add column password

--- database/migrations/2024_05_29_020251_create_institutions_table.php
+++ database/migrations/2024_05_29_020251_create_institutions_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->integer('phone_number');
+            $table->string('password');
             $table->timestamps();
         });
     }
