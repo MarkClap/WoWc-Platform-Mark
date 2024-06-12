2b9ba7bf3833b7233ae04be054d4d42b1aac0a92
Mark_clap <blusito48@gmail.com>
2024-06-11 22:33:29 -0500

    create teacher_courses_quizzes table

---
+++ database/migrations/2024_06_12_033120_create_teacher_courses_quizzes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_courses_quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_teacher_course');
+            $table->unsignedBigInteger('id_quiz');
+            $table->timestamps();
+
+            $table->foreign('id_teacher_course')->references('id')->on('teachers_courses');
+            $table->foreign('id_quiz')->references('id')->on('quizzes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_courses_quizzes');
+    }
+};
