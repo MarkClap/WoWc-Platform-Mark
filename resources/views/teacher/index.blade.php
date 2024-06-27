3b815d622d354b5935a85fdb1f751cff3546eca6
Fernando-primeros-pasos <fernando.chocce@tecsup.edu.pe>
2024-06-26 19:28:03 -0500

    adding styles to the teacher dashboard

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,54 +1,67 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="flex h-screen bg-white">
+    <div class="bg-white-400 flex w-full justify-center items-center">
+                
+                <div class="px-3 py-4 flex w-8/12 bg-red-400 flex-col items-center">
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-                            <span id="card_title">
+                    {{-- content-title --}}
+                    <div class="p-3 flex justify-between w-11/12 items-center ">
+                        
+                            <span class="text-2xl text-black ">
                                 {{ __('Teachers') }}
                             </span>
-                            <div class="float-right">
-                                <a href="{{ route('teachers.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                            <div class="">
+                                <a href="{{ route('teachers.create') }}" class=" text-md bg-blue-500 hover:bg-blue-700 text-white p-2 rounded focus:outline-none focus:shadow-outline">
                                 {{ __('Create New') }}
                                 </a>
                             </div>
-                        </div>
+                        
                     </div>
+                    {{-- content-title --}}
+                    
                     @if ($message = Session::get('success'))
-                        <div class="alert alert-success m-4">
-                            <p>{{ $message }}</p>
-                        </div>
+                        
+                        <div role="alert" class="alert alert-success">
+                            <span class="icon-[ep--success-filled] text-2xl"></span>
+                            <p class="text-xl text-black">{{ $message }}</p>
+                          </div>
                     @endif
-                    <div class="card-body bg-white">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
+                    
+                    <div class="rounded-sm flex w-full items-center overflow-x-scroll">
+                        
+                            <table class="w-full text-md bg-white shadow-md rounded">
+                                <thead class="border-b text-black">
                                     <tr>
-                                        <th>Id</th>
                                         
-									<th >Teacher</th>
-									<th >Email</th>
-									<th >Institution</th>
+									<th class="text-left p-3 px-5" >Teacher</th>
+									<th class="text-left p-3 px-5">Email</th>
+									<th class="text-left p-3 px-5">Institution</th>
                                         <th></th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class=" text-black space-y-2">
                                     @foreach ($teachers as $teacher)
                                         <tr>
-                                            <td>{{ $teacher->user->id }}</td>
-                                            
-										<td >{{ $teacher->user->name }}</td>
-										<td >{{ $teacher->user->email }}</td>
-										<td >{{ $teacher->institution->name }}</td>
-                                            <td>
-                                                <form action="{{ route('teachers.destroy', $teacher->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('teachers.show', $teacher->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('teachers.edit', $teacher->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                
+                                            <td class="text-left p-3 px-5">{{ $teacher->user->name }}</td>
+                                            <td class="text-left p-3 px-5" >{{ $teacher->user->email }}</td>
+                                            <td class="text-left p-3 px-5">{{ $teacher->institution->name }}</td>
+                                            <td class="p-3 px-5 text-left">
+                                                <form action="{{ route('teachers.destroy', $teacher->id) }}" method="POST" class="flex flex-row justify-center items-center mb-0 gap-4" >
+                                                    <a class="bg-yellow-200 hover:bg-yellow border-2 border-black rounded-md p-2" href="{{ route('teachers.show', $teacher->id) }}"><span class="icon-[bx--show]"></span></a>
+                                                    <a class="bg-green-200 hover:bg-green-700 border-2 border-black rounded-md p-2" href="{{ route('teachers.edit', $teacher->id) }}"><span class="icon-[carbon--edit]"></span></a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="bg-red-500 hover:bg-red-700 border-2 border-black rounded-md p-2" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><span class="icon-[mi--delete]"></span></button>
                                                 </form>
                                             </td>
                                         </tr>
@@ -56,10 +69,16 @@
                                 </tbody>
                             </table>
                         </div>
+
+                        
                     </div>
                 </div>
                 {!! $teachers->withQueryString()->links() !!}
-            </div>
+           
         </div>
-    </div>
+    
 
+
+</body>
+</html>
+    
