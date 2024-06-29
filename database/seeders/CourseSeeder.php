fe29de08061c130b95c6e424a259cd74063194d9
Mark_clap <blusito48@gmail.com>
2024-06-28 22:47:20 -0500

    add some seeders

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Course;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Course::create([
+            'name' => 'Big Data',
+            'description' => 'description big data',
+        ]);
+
+        Course::create([
+            'name' => 'Database',
+            'description' => 'description database',
+        ]);
+
+        Course::create([
+            'name' => 'Math',
+            'description' => 'description math',
+        ]);
+    }
+}
